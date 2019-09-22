b35e5f12f06620ddcad9b09cff30bd143df49538
flood <kansini@gmail.com>
2019-09-22 14:24:13 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,33 @@
     <div class="home">
         <div class="logo"></div>
         <div class="mainNav">
-            <div class="left"></div>
+            <div class="left">
+                <div class="video"></div>
+            </div>
             <div class="right">
                 <div class="row">
                     <router-link tag="div" to="/" class="nav-item-01">
                         区域化党建
                     </router-link>
-                    <div class="nav-item-02"></div>
+                    <div class="nav-item-02">
+                        要闻速递
+                    </div>
                 </div>
                 <div class="row">
-                    <div class="nav-item-03"></div>
+                    <div class="nav-item-03">
+                        “两新”党建
+                    </div>
                     <div class="nav-item-right">
-                        <div class="nav-item-04"></div>
-                        <div class="nav-item-05"></div>
+                        <div class="nav-item-04">
+                            宣传统战
+                        </div>
+                        <div class="nav-item-05">
+                            留言堂
+                        </div>
                         <div class="row">
-                            <div class="nav-item-06"></div>
+                            <div class="nav-item-06">
+                                社会组织党建
+                            </div>
                         </div>
                     </div>
 
@@ -67,8 +79,21 @@
             .left {
                 width: calc(50% - calc(24rem / 96));
                 height: 100%;
-                background: #42b983;
                 margin-right: calc(24rem / 96);
+                overflow: hidden;
+                padding: calc(16rem / 96);
+                background: #fff;
+                box-sizing: border-box;
+                border-radius: 8px;
+
+
+                .video {
+                    width: 100%;
+                    height: 100%;
+                    background: url("../assets/img/video.png") no-repeat center;
+                    background-size: auto 100%;
+                    border-radius: 6px;
+                }
             }
 
             .right {
@@ -86,10 +111,21 @@
 
                     [class^="nav-item-"] {
                         float: left;
+                        position: relative;
                         border-radius: 10px;
                         color: #fff;
                         font-size: calc(28rem / 96);
                         cursor: pointer;
+                        text-align: center;
+                        overflow: hidden;
+
+                        &::before,
+                        &::after {
+                            position: absolute;
+                            content: '';
+                            background: rgba(255, 255, 255, .1);
+                            border-radius: 100%;
+                        }
                     }
 
                     .nav-item-01 {
@@ -100,33 +136,89 @@
                         background: #3cadee;
                         margin-bottom: calc(24rem / 96);
 
+                        &::before {
+                            top: calc(-48rem / 96);
+                            left: calc(118rem / 96);
+                            width: calc(230rem / 96);
+                            height: calc(360rem / 96);
+                            transform: rotate(15deg);
+                        }
+
+                        &::after {
+                            top: calc(96rem / 96);
+                            right: calc(-190rem / 96);
+                            width: calc(270rem / 96);
+                            height: calc(364rem / 96);
+                            transform: rotate(15deg);
+                        }
+
+
                     }
 
                     .nav-item-02 {
                         width: calc(198rem / 96);
                         height: calc(230rem / 96);
+                        line-height: calc(320rem / 96);
                         background: #f7ae6c;
                         margin-bottom: calc(24rem / 96);
+
+                        &::after {
+                            top: calc(96rem / 96);
+                            left: calc(-108rem / 96);
+                            width: calc(270rem / 96);
+                            height: calc(364rem / 96);
+                            transform: rotate(20deg);
+                        }
+
                     }
 
                     .nav-item-03 {
                         width: calc(314rem / 96);
                         height: calc(346rem / 96);
+                        line-height: calc(500rem / 96);
                         margin-right: calc(24rem / 96);
                         background: #f07879;
+
+                        &::before {
+                            top: calc(-298rem / 96);
+                            left: calc(118rem / 96);
+                            width: calc(230rem / 96);
+                            height: calc(360rem / 96);
+                            transform: rotate(15deg);
+                        }
+
+                        &::after {
+                            top: calc(38rem / 96);
+                            left: calc(18rem / 96);
+                            width: calc(406rem / 96);
+                            height: calc(248rem / 96);
+                            transform: rotate(25deg);
+                        }
+
+
                     }
 
                     .nav-item-04 {
                         width: calc(198rem / 96);
                         height: calc(166rem / 96);
+                        line-height: calc(240rem / 96);
                         margin-right: calc(24rem / 96);
                         background: #3fcaac;
                         margin-bottom: calc(24rem / 96);
+
+                        &::after {
+                            top: calc(58rem / 96);
+                            left: calc(-288rem / 96);
+                            width: calc(406rem / 96);
+                            height: calc(248rem / 96);
+                            transform: rotate(25deg);
+                        }
                     }
 
                     .nav-item-05 {
                         width: calc(198rem / 96);
                         height: calc(166rem / 96);
+                        line-height: calc(240rem / 96);
                         background: #8479f1;
                         margin-bottom: calc(24rem / 96);
                     }
@@ -134,7 +226,16 @@
                     .nav-item-06 {
                         width: calc(420rem / 96);
                         height: calc(152rem / 96);
+                        line-height: calc(152rem / 96);
                         background: #ce87c2;
+
+                        &::after {
+                            top: calc(-160rem / 96);
+                            left: calc(-300rem / 96);
+                            width: calc(406rem / 96);
+                            height: calc(248rem / 96);
+                            transform: rotate(25deg);
+                        }
                     }
 
                 }
